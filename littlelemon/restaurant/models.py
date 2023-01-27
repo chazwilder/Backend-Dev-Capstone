fad626bcdee97aca50c0590292d53f761b2a3082
Chaz <cwilder@chazwilder.onmicrosoft.com>
2023-01-27 15:23:48 -0500

    Adding API Funtionality for CRUD operations

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -8,9 +8,16 @@ class Booking(models.Model):
     Name = models.CharField(max_length=255)
     No_Of_Guests = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(6)])
     BookingDate = models.DateTimeField()
+    
+    def __str__(self):
+        return f'{self.Name} : {str(self.ID)}'
+    
 
 class Menu(models.Model):
     ID = models.AutoField(auto_created = True,primary_key=True)
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(decimal_places=2,max_digits=10)
     Inventory = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.Title} : {str(self.Price)}'
--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Menu,Booking
+from .serializers import MenuSerializer,BookingSerializer
+from .views import MenuItemsView,BookingViewSet
 
-# Create your tests here.
+#TestCase class
+class MenuTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(Title="IceCream", Price=80, Inventory=100)
+        self.assertEqual(str(item), "IceCream : 80")
+        
+        
+class MenuViewTest(TestCase):
+    def setUp(self):
+        self.cParm = Menu.objects.create(Title="Chicken Parm",Price=12,Inventory=100)
+        self.cParm = Menu.objects.create(Title="Fried Fish",Price=16,Inventory=50)
+        
+    def test_getall(self):
+        menuItems = Menu.objects.all()
+        serialized = MenuSerializer(menuItems,many=True)
+        print(str(serialized.data))
+        self.assertEquals(str(serialized.data), "[OrderedDict([('ID', 2), ('Title', 'Chicken Parm'), ('Price', '12.00'), ('Inventory', 100)]), OrderedDict([('ID', 3), ('Title', 'Fried Fish'), ('Price', '16.00'), ('Inventory', 50)])]")
+    
+
+        
